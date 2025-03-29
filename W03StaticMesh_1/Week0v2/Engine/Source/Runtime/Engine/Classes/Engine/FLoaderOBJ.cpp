49e6447acd4ab4a25c5ea49bf6d3f7edd8831659
hans4809 <58892107+hans4809@users.noreply.github.com>
2025-03-29 17:20:54 +0900

    Update FLoaderOBJ.cpp

--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Engine/Classes/Engine/FLoaderOBJ.cpp
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Engine/Classes/Engine/FLoaderOBJ.cpp
@@ -329,7 +329,7 @@ bool FLoaderOBJ::ConvertToStaticMesh(const FObjInfo& RawData, OBJ::FStaticMeshRe
             vertex.y = RawData.Vertices[vIdx].y;
             vertex.z = RawData.Vertices[vIdx].z;
 
-            vertex.r = 1.0f; vertex.g = 1.0f; vertex.b = 1.0f; vertex.a = 1.0f; // 기본 색상
+            //vertex.r = 1.0f; vertex.g = 1.0f; vertex.b = 1.0f; vertex.a = 1.0f; // 기본 색상
 
             if (tIdx != UINT32_MAX && tIdx < RawData.UVs.Num())
             {
@@ -337,22 +337,22 @@ bool FLoaderOBJ::ConvertToStaticMesh(const FObjInfo& RawData, OBJ::FStaticMeshRe
                 vertex.v = -RawData.UVs[tIdx].y;
             }
 
-            if (nIdx != UINT32_MAX && nIdx < RawData.Normals.Num())
-            {
-                vertex.nx = RawData.Normals[nIdx].x;
-                vertex.ny = RawData.Normals[nIdx].y;
-                vertex.nz = RawData.Normals[nIdx].z;
-            }
+            // if (nIdx != UINT32_MAX && nIdx < RawData.Normals.Num())
+            // {
+            //     vertex.nx = RawData.Normals[nIdx].x;
+            //     vertex.ny = RawData.Normals[nIdx].y;
+            //     vertex.nz = RawData.Normals[nIdx].z;
+            // }
 
-            for (int32 j = 0; j < OutStaticMesh.MaterialSubsets.Num(); j++)
-            {
-                const FMaterialSubset& subset = OutStaticMesh.MaterialSubsets[j];
-                if ( i >= subset.IndexStart && i < subset.IndexStart + subset.IndexCount)
-                {
-                    vertex.MaterialIndex = subset.MaterialIndex;
-                    break;
-                }
-            }
+            // for (int32 j = 0; j < OutStaticMesh.MaterialSubsets.Num(); j++)
+            // {
+            //     const FMaterialSubset& subset = OutStaticMesh.MaterialSubsets[j];
+            //     if ( i >= subset.IndexStart && i < subset.IndexStart + subset.IndexCount)
+            //     {
+            //         vertex.MaterialIndex = subset.MaterialIndex;
+            //         break;
+            //     }
+            // }
             
             index = OutStaticMesh.Vertices.Num();
             OutStaticMesh.Vertices.Add(vertex);
